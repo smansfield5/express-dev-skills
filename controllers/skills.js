39cae4ea443dbbc3462d8017eef0b4ad8247725f
smansfield5 <smansfield5@yahoo.com>
2022-04-15 16:40:53 -0600

    updated

---
+++ controllers/skills.js
@@ -0,0 +1,48 @@
+const Skill = require('../models/skill');
+const { render } = require('../server')
+
+module.exports = {
+    index,
+    show,
+    new: newSkill,
+    create,
+    delete: deleteSkill,
+    edit,
+    update
+};
+
+function index(req, res) {
+    const skills = Skill.getAll();
+    res.render('skills/index', { skills });
+}
+
+function update(req, res) {
+    req.body.done = !!req.body.done;
+    Skill.update(req.params.id, req.body);
+    res.redirect(`/skills/${req.params.id}`);
+}
+
+function edit(req, res) {
+    const skill = Skill.getOne(req.params.id);
+    res.render('/skills/edit', { skill });
+}
+
+function deleteSkill(req, res) {
+    Skill.deleteOne(req.params.id);
+    res.redirect('/skills')
+}
+
+function create(req, res) {
+    Skill.create(req.body)
+    res.redirect('/skills')
+}
+
+function newSkill(req, res) {
+    res.render('skills/new');
+}
+
+function show(req, res) {
+    const skill = Skill.getOne(req.params.id);
+    res.render('skills/show', {skill})
+}
+
--- models/skill.js
+++ models/skill.js
@@ -1,12 +1,39 @@
 const skills = [
-    {skill: 'coding'},
-    {skill: 'functions'},
-    {skill: 'loops'}
+    {id: 555555, skill: 'coding', done: false},
+    {id: 333333, skill: 'functions', done: false},
+    {id: 222222, skill: 'loops', done: false}
 ];
 
 module.exports = {
-    getAll
+    getAll,
+    getOne, 
+    create,
+    deleteOne,
+    update
 };
+
+function update(id, skill) {
+    id = parseInt(id);
+    const updatedSkill = skills.find((skill) => skill.id === id);
+    Object.assign(updatedSkill, skill);
+}
+
+function deleteOne(id) {
+    id = parseInt(id);
+    const idx = skills.findIndex((skill) => skill.id === id);
+    skill.splice(idx, 1);
+}
+
+function create(skill) {
+    skill.id = Date.now() % 1000000;
+    skill.done = false;
+    skills.push(skill);
+}
+
+function getOne(id) {
+    id = parseInt(id);
+    return skills.find((skill) => skill.id === id);
+}
 
 function getAll() {
     return skills;
